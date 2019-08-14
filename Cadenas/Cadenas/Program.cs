0cfa719e431a106b46c039e35ca63d44b1da6aa7
dennyvim <41881945+dennyvim@users.noreply.github.com>
2019-08-14 07:09:54 -0500

    Add files via upload

---
+++ Cadenas/Cadenas/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadenas
+{
+    class Program
+    {
+
+        static void generarCarater(int lon) {
+
+
+
+            Guid miGuid = Guid.NewGuid();
+            string token = Convert.ToBase64String(miGuid.ToByteArray());
+            token = token.Replace("=", "").Replace("+", "").Replace("/", "");
+            if (lon > 4)
+            {
+
+                token = token.Insert(0, "a");
+            }
+
+            Console.WriteLine(token.Substring(0, lon));
+
+            
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+
+
+            int lon, i, j;
+
+            for (i = 0; i <= 1000; i++) {
+
+                Console.WriteLine(i);
+                lon = r.Next(2, 10);
+                generarCarater(lon);
+            }
+                
+
+        }
+    }
+}
